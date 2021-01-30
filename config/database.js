736818321554d818011745272da8bf8cae2f6deb
Sangsaka Wira <sangsakawira@Sangsakas-MacBook-Pro.local>
2021-01-30 18:11:51 +0700

    docker example sql setup added

--- config/database.js
+++ config/database.js
@@ -2,9 +2,10 @@ const mysql = require("mysql2")
 
 const pool = mysql.createPool({
     host:"localhost",
+    port:"5555",
     user:"root",
-    database:"users",
-    password:"Forz4tano"
+    database:"karya_ibu",
+    password:"yoursecret"
 })
 
 module.exports = pool.promise()
